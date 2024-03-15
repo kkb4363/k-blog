faa2954afbe382045a7f1f3622370355f455fcc7
gibeom <101088024+kkb4363@users.noreply.github.com>
2024-03-15 18:56:50 +0800

    페이지전환애니메이션 해야함

--- src/pages/Pc.tsx
+++ src/pages/Pc.tsx
@@ -1,19 +1,17 @@
 import styled from "styled-components";
 import logoIcon from "../assets/logo.png";
 import Container from "../components/Container";
-import lineIcon from "../assets/line.svg";
-import circleIcon from "../assets/circle.svg";
-import shopIcon from "../assets/shop.svg";
-import tempIcon from "../assets/tmp1.png";
+
 import { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const PcBox = styled.div`
-  width: 100vw;
-  height: 100vh;
   overflow: scroll;
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 100vw;
+  height: 100vh;
 `;
 
 const Header = styled.div`
@@ -85,134 +83,8 @@ const Text1 = styled.div`
   }
 `;
 
-const Body = styled.div`
-  width: 60%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: 10vh;
-  overflow: auto;
-
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-`;
-
-const Title = styled.div`
-  position: relative;
-  & > p {
-    font-size: 70px;
-    font-weight: 00;
-    line-height: 1.1;
-  }
-  margin-bottom: 10vh;
-`;
-
-const Line1 = styled.img`
-  position: absolute;
-  width: 40%;
-  left: -20px;
-  top: -10px;
-  z-index: -1;
-  transform: rotate(15deg);
-`;
-
-const Line2 = styled.img`
-  position: absolute;
-  width: 65%;
-  right: 0px;
-  top: -20px;
-  z-index: -1;
-  transform: rotate(15deg);
-`;
-
-const BlogTitleBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  min-height: 150px;
-  border-top: 2px solid black;
-  border-bottom: 2px solid black;
-`;
-
-const BlogTitleRow = styled.div`
-  width: 100%;
-  height: 50%;
-  display: flex;
-`;
-
-const ShopIcon = styled.img`
-  width: 5%;
-  height: 100%;
-  background-color: black;
-`;
-
-const BlogTitleName = styled.div`
-  width: 95%;
-  display: flex;
-  align-items: flex-end;
-  padding-left: 30px;
-
-  & > span {
-    font-size: 50px;
-    font-weight: 600;
-  }
-`;
-
-const BlogTitleRow2 = styled(BlogTitleRow)`
-  & > span {
-    padding-left: calc(35px + 5%);
-    padding-top: 10px;
-    font-weight: 500;
-    font-size: 20px;
-  }
-  justify-content: space-between;
-  & > div {
-    cursor: pointer;
-    align-self: flex-end;
-    width: 12%;
-    min-height: 50px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #000000;
-    color: #ffffff;
-
-    &:hover {
-      background-color: #333333;
-    }
-
-    & > span {
-      font-size: 18px;
-      font-weight: 500;
-    }
-  }
-`;
-
-const BlogContentRow = styled.div`
-  width: 100%;
-  padding: 30px 0;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-`;
-
-const BlogItemBox = styled.div`
-  width: 298px;
-  height: 268px;
-  display: flex;
-  flex-direction: column;
-  background-color: rgb(195, 255, 91);
-  & > img {
-    height: 60%;
-    object-fit: cover;
-  }
-
-  & > div {
-    padding: 20px 10px 10px 10px;
-  }
-`;
-
 export default function Pc() {
+  const navigate = useNavigate();
   const [hideHeader, setHideHeader] = useState(false);
 
   const handleScroll = () => {
@@ -221,6 +93,7 @@ export default function Pc() {
     } else {
       setHideHeader(false);
     }
+    console.log(12);
   };
   useEffect(() => {
     document?.getElementById("test")?.addEventListener("scroll", handleScroll);
@@ -232,109 +105,32 @@ export default function Pc() {
   }, []);
 
   return (
-    <Container>
-      <PcBox>
-        {!hideHeader && (
-          <Container>
-            <Header>
-              <Col>
-                <Row>
-                  <Left>
-                    <Logo>
-                      <img src={logoIcon} />
-                    </Logo>
-                    <Text1>
-                      <p>K-Blog</p>
-                      <p>Hope Happiness</p>
-                    </Text1>
-                  </Left>
-                  <Right>
-                    <span>Home</span>
-                    <span>Blog</span>
-                    <span>Login</span>
-                  </Right>
-                </Row>
-              </Col>
-            </Header>
-          </Container>
-        )}
-        <Body id="test">
-          <Title>
-            <Line1 src={lineIcon} />
-            <Line2 src={circleIcon} />
-            <p>Slow down</p>
-            <p>and Enjoy life</p>
-          </Title>
-          <BlogTitleBox>
-            <BlogTitleRow>
-              <ShopIcon src={shopIcon} />
-              <BlogTitleName>
-                <span>Fitness</span>
-              </BlogTitleName>
-            </BlogTitleRow>
-            <BlogTitleRow2>
-              <span>My secret workout plan</span>
-              <div>
-                <span>Read More</span>
-              </div>
-            </BlogTitleRow2>
-          </BlogTitleBox>
-          <BlogContentRow>
-            <BlogItemBox>
-              <img src={tempIcon} />
-              <div>
-                <span>six simple high intensity workouts test</span>
-              </div>
-            </BlogItemBox>
-            <BlogItemBox>
-              <img src={tempIcon} />
-              <div>
-                <span>six simple high intensity workouts test</span>
-              </div>
-            </BlogItemBox>
-            <BlogItemBox>
-              <img src={tempIcon} />
-              <div>
-                <span>six simple high intensity workouts test</span>
-              </div>
-            </BlogItemBox>{" "}
-          </BlogContentRow>
-          <BlogTitleBox>
-            <BlogTitleRow>
-              <ShopIcon src={shopIcon} />
-              <BlogTitleName>
-                <span>Fitness</span>
-              </BlogTitleName>
-            </BlogTitleRow>
-            <BlogTitleRow2>
-              <span>My secret workout plan</span>
-              <div>
-                <span>Read More</span>
-              </div>
-            </BlogTitleRow2>
-          </BlogTitleBox>
-          <BlogContentRow>
-            <BlogItemBox>
-              <img src={tempIcon} />
-              <div>
-                <span>six simple high intensity workouts test</span>
-              </div>
-            </BlogItemBox>
-            <BlogItemBox>
-              <img src={tempIcon} />
-              <div>
-                <span>six simple high intensity workouts test</span>
-              </div>
-            </BlogItemBox>
-            <BlogItemBox>
-              <img src={tempIcon} />
-              <div>
-                <span>six simple high intensity workouts test</span>
-              </div>
-            </BlogItemBox>{" "}
-          </BlogContentRow>
-        </Body>
-      </PcBox>
-    </Container>
+    <PcBox>
+      {!hideHeader && (
+        <Container>
+          <Header>
+            <Col>
+              <Row>
+                <Left>
+                  <Logo>
+                    <img src={logoIcon} />
+                  </Logo>
+                  <Text1>
+                    <p>K-Blog</p>
+                    <p>Hope Happiness</p>
+                  </Text1>
+                </Left>
+                <Right>
+                  <span onClick={() => navigate("")}>Home</span>
+                  <span onClick={() => navigate("blogs")}>Blog</span>
+                  <span>Login</span>
+                </Right>
+              </Row>
+            </Col>
+          </Header>
+        </Container>
+      )}
+      <Outlet />
+    </PcBox>
   );
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
+import Main from "./components/home/Main";
+import Blog from "./components/home/Blog";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +12,16 @@ export const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+        children: [
+          {
+            path: "",
+            element: <Main />,
+          },
+          {
+            path: "blogs",
+            element: <Blog />,
+          },
+        ],
       },
     ],
   },
