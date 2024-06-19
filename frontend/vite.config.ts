a1403c996526dc9e8f68d22b48e2eb52a20a4f31
98_gb <kkb4363@naver.com>
2024-06-19 18:01:57 +0900

    bb

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://k-blog-snowy.vercel.app",
         changeOrigin: true,
       },
     },
